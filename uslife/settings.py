3749cd7041da76973dead85f32a437f846a488ed
yansong10101 <yansong10101@gmail.com>
2015-11-13 16:51:20 -0800

    new project initialize step 1

--- uslife/settings.py
+++ uslife/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DEVELOPMENT = os.path.exists("/Users/zys")
 
 # Application definition
 
@@ -75,20 +76,24 @@ WSGI_APPLICATION = 'uslife.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
-    'default': {
-        'NAME': 'test',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': 'newpwd',
-        'HOST': 'localhost',
-        'PORT': '3306',
+if DEVELOPMENT:
+    DATABASES = {
+        'default': {
+            'NAME': 'test',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'root',
+            'PASSWORD': 'newpwd',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'default': dj_database_url.parse(
+            os.environ.get('DATABASE_URL', None))
+        }
+    }
 
 
 # Internationalization
