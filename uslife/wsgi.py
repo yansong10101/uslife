ade87d9b0571b04e5791fd3c788145c9bb12fb84
yansong10101 <yansong10101@gmail.com>
2016-01-30 12:09:20 -0800

    add req python-binary-memcached==0.24.3

--- uslife/wsgi.py
+++ uslife/wsgi.py
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
-from django.core.cache.backends.memcached import BaseMemcachedCache
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uslife.settings")
+
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 application = Cling(get_wsgi_application())
 
+
 # Fix django closing connection to MemCachier after every request (#11331)
+from django.core.cache.backends.memcached import BaseMemcachedCache
 BaseMemcachedCache.close = lambda self, **kwargs: None
